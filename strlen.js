cb79f5981f95930e46e2d0634e90484d87888657
Dave Syer <dsyer@vmware.com>
2022-07-07 12:57:56 +0000

    Push memory manipulation into library

--- strlen.js
+++ strlen.js
@@ -1,7 +1,18 @@
-import fs from 'fs';
-import path from 'path';
-const file = fs.readFileSync(path.dirname(import.meta.url).replace("file://", "") + '/strlen.wasm');
-let wasm = await WebAssembly.instantiate(file, {});
+async function bytes(file) {
+	if (typeof fetch !== "undefined") {
+		return await fetch(file).then(response => response.arrayBuffer());
+	}
+    const path = await import('path');
+	return await import('fs').then(fs => fs.readFileSync(path.dirname(import.meta.url).replace("file://", "") + "/" + file));
+}
+
+let wasm = await WebAssembly.instantiate(await bytes("strlen.wasm"), {});
 let { string_length, memory } = wasm.instance.exports;
 
-export { wasm, string_length };
+const encoder = new TextEncoder();
+
+export function strlen(str) {
+    new Uint8Array(memory.buffer, 0, str.length + 1).set(encoder.encode(str));
+    const result =string_length(0);
+    return result;
+};
--- tests/index.js
+++ tests/index.js
@@ -1,16 +1,9 @@
 import assert from "assert";
-import { wasm, string_length } from "../strlen.js"
-
-const encoder = new TextEncoder();
-const decoder = new TextDecoder();
+import { strlen } from "../strlen.js"
 
 async function run() {
 
-    const { memory } = wasm.instance.exports;
-
-    const name = "World";
-    new Uint8Array(memory.buffer, 0, name.length + 1).set(encoder.encode(name));
-    const result =string_length(0);
+    const result = strlen("Hello");
     console.log("Length:", result);
     assert.strictEqual(result, 5);
 
